21dc0567459f35537f25ff08200c1cec915e8567
Rn246 <raynery246@gmail.com>
2023-11-16 18:41:44 -0300

    Enemy HitBox added

--- ProjectApollo III/GameViewModel.swift
+++ ProjectApollo III/GameViewModel.swift
@@ -2,7 +2,7 @@ import SpriteKit
 import SwiftUI
 import GameKit
 
-class GameScene: SKScene, ObservableObject {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     private var videoNode: SKVideoNode!
     let background = SKSpriteNode(imageNamed: "nebula")
@@ -14,8 +14,16 @@ class GameScene: SKScene, ObservableObject {
     var dougTimer = Timer()
     var enemyTimer = Timer()
     
+    struct CBitmask{
+        static let playerBody: UInt32 = 0b1
+        static let playerAttack: UInt32 = 0b10
+        static let enemyBody: UInt32 = 0b100
+        
+    }
+    
     
     override func didMove(to view: SKView) {
+        physicsWorld.contactDelegate = self
         scene?.size = CGSize(width: 750, height: 1335)
         background.position = CGPoint(x: size.width / 2, y:  size.height / 2)
         background.setScale(1.5)
@@ -23,9 +31,31 @@ class GameScene: SKScene, ObservableObject {
         addChild(background)
         makePlayer(playerCh: 1)
         //DougPowerz()
-        enemyTimer = .scheduledTimer(timeInterval: 2, target: self, selector: #selector(makeEnemy), userInfo: nil, repeats: true)
+        enemyTimer = .scheduledTimer(timeInterval: 1, target: self, selector: #selector(makeEnemy), userInfo: nil, repeats: true)
         fireTimer = .scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(playerFireFunc), userInfo: nil, repeats: true)
         //        dougTimer = .scheduledTimer(timeInterval: 20.0, invocation: NSInvocation, repeats: true)
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let contact1 : SKPhysicsBody
+        let contact2 : SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            contact1 = contact.bodyA
+            contact2 = contact.bodyB
+        } else {
+            contact1 = contact.bodyB
+            contact2 = contact.bodyA
+        }
+        if contact1.categoryBitMask == CBitmask.playerAttack && contact2.categoryBitMask == CBitmask.enemyBody {
+            enemyDestroied(fire: contact1.node as! SKSpriteNode, enemy: contact2.node as! SKSpriteNode)
+        }
+    }
+    
+    func enemyDestroied(fire: SKSpriteNode, enemy: SKSpriteNode){
+        fire.removeFromParent()
+        enemy.removeFromParent()
+        
     }
     
     func makePlayer(playerCh: Int) {
@@ -45,6 +75,12 @@ class GameScene: SKScene, ObservableObject {
         player.position = CGPoint(x: size.width / 2, y: 120)
         player.zPosition = 10
         player.setScale(1.5)
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.affectedByGravity = false
+        player.physicsBody?.isDynamic = true
+        player.physicsBody?.categoryBitMask = CBitmask.playerBody
+        player.physicsBody?.contactTestBitMask = CBitmask.enemyBody
+        player.physicsBody?.collisionBitMask = CBitmask.enemyBody
         addChild(player)
     }
     
@@ -57,18 +93,28 @@ class GameScene: SKScene, ObservableObject {
         playerFire.position = player.position
         playerFire.zPosition = 3
         playerFire.setScale(5.0)
+        playerFire.physicsBody = SKPhysicsBody(rectangleOf: playerFire.size)
+        playerFire.physicsBody?.affectedByGravity = false
+        playerFire.physicsBody?.categoryBitMask = CBitmask.playerAttack
+        playerFire.physicsBody?.contactTestBitMask = CBitmask.enemyBody
+        playerFire.physicsBody?.collisionBitMask = CBitmask.enemyBody
         addChild(playerFire)
         playerFire.run(combine)
     }
     
     func randomPoint() -> Int {
-            return Int.random(in: 30...1400)
+            return Int.random(in: 50...1350)
         }
     
     @objc func makeEnemy() {
         enemy = .init(imageNamed: "enemyShip1")
         enemy.position = CGPoint(x: randomPoint(), y: 1200)
         enemy.zPosition = 5
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody?.affectedByGravity = false
+        enemy.physicsBody?.categoryBitMask = CBitmask.enemyBody
+        enemy.physicsBody?.contactTestBitMask = CBitmask.playerBody | CBitmask.playerAttack
+        enemy.physicsBody?.collisionBitMask = CBitmask.playerAttack | CBitmask.playerAttack
         addChild(enemy)
         
         let moveAction = SKAction.moveTo(y: -100, duration: 3)
@@ -87,17 +133,5 @@ class GameScene: SKScene, ObservableObject {
         }
     }
     
-    //    func DougPowerz() {
-    //        dougPower = .init(imageNamed:"dougPower")
-    //        dougPower.position = CGPoint(x: size.width / 2, y: 1020)
-    //        dougPower.zPosition = 10
-    //        dougPower.setScale(1.5)
-    //        addChild(dougPower)
-    //    }
-}
 
-//        let video = SKVideoNode(fileNamed: "video")
-//        video.size = CGSize(width: 750, height: 1335)
-//        addChild(video)
-//        videoNode.position = CGPoint(x: frame.midX, y: frame.midY)
-//        videoNode.play()
+}
